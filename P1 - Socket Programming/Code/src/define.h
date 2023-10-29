68191910172993c214fadbedaae9b6e6a596280f
Shahriar-0 <attarshahriar@gmail.com>
2023-10-29 12:19:24 -0400

    add defines

---
+++ P1 - Socket Programming/Code/src/define.h
@@ -0,0 +1,31 @@
+#ifndef DEFINE_H_INCLUDE
+#define DEFINE_H_INCLUDE
+
+// includes
+#include <arpa/inet.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+// Constants
+#define BUF_NAME  32
+#define BUF_PNAME 64
+#define BUF_CLI   128
+#define BUF_MSG   512
+#define BCAST_IP  "255.255.255.255"
+#define TIMEOUT   90
+
+
+// Structs
+//TODO
+
+#endif
