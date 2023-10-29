9dd4f3d0c7863f89e78eec75b5cd1168c4d9381a
Shahriar-0 <attarshahriar@gmail.com>
2023-10-29 12:11:56 -0400

    add base files

---
+++ P1 - Socket Programming/Code/src/ansi_colors.h
@@ -0,0 +1,31 @@
+#ifndef ANSI_COLORS_H_INCLUDE
+#define ANSI_COLORS_H_INCLUDE
+
+#define ANSI_RED "\x1B[31m"
+#define ANSI_GRN "\x1B[32m"
+#define ANSI_YEL "\x1B[33m"
+#define ANSI_BLU "\x1B[34m"
+#define ANSI_MAG "\x1B[35m"
+#define ANSI_CYN "\x1B[36m"
+#define ANSI_WHT "\x1B[37m"
+
+#define ANSI_BRED "\x1B[1;31m"
+#define ANSI_BGRN "\x1B[1;32m"
+#define ANSI_BYEL "\x1B[1;33m"
+#define ANSI_BBLU "\x1B[1;34m"
+#define ANSI_BMAG "\x1B[1;35m"
+#define ANSI_BCYN "\x1B[1;36m"
+#define ANSI_BWHT "\x1B[1;37m"
+
+#define ANSI_BKG_RED "\x1B[41m"
+#define ANSI_BKG_GRN "\x1B[42m"
+#define ANSI_BKG_YEL "\x1B[43m"
+#define ANSI_BKG_BLU "\x1B[44m"
+#define ANSI_BKG_MAG "\x1B[45m"
+#define ANSI_BKG_CYN "\x1B[46m"
+#define ANSI_BKG_WHT "\x1B[47m"
+
+#define ANSI_RST "\x1B[0m"
+#define ANSI_LEN 9
+
+#endif  // ANSI_COLORS_H_INCLUDE
---
+++ P1 - Socket Programming/Code/src/sample_bitch.c
@@ -0,0 +1,141 @@
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+int make_broad_sock() {
+    int sock, broadcast = 1, opt = 1;
+    char buffer[1024] = {0};
+    struct sockaddr_in bc_address;
+
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast));
+    setsockopt(sock, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt));
+
+    return (sock);
+}
+
+struct sockaddr_in make_broad_addr() {
+    struct sockaddr_in bc_address;
+    bc_address.sin_family = AF_INET;
+    bc_address.sin_port = htons(8080);
+    bc_address.sin_addr.s_addr = inet_addr("255.255.255.255");
+
+    return (bc_address);
+}
+
+int setupServer(int port) {
+    struct sockaddr_in address;
+    int server_fd;
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    
+    listen(server_fd, 4);
+
+    return server_fd;
+}
+
+int acceptClient(int server_fd) {
+    int client_fd;
+    struct sockaddr_in client_address;
+    int address_len = sizeof(client_address);
+    client_fd = accept(server_fd, (struct sockaddr *)&client_address, (socklen_t*) &address_len);
+
+    return client_fd;
+}
+
+
+
+
+
+
+int main(int argc, char const *argv[]) {
+    int udp_conn, tcp_conn, max_sd;
+    char buffer[1024] = {0};
+    struct sockaddr_in main_addr;
+
+    fd_set rd_set, master_set;
+
+    main_addr = make_broad_addr();
+
+    tcp_conn = setupServer(atoi(argv[1]));
+    udp_conn = make_broad_sock();
+
+    bind(udp_conn, (struct sockaddr *)&main_addr, sizeof(main_addr));
+
+    max_sd = udp_conn;
+    FD_ZERO(&master_set);
+    FD_SET(STDIN_FILENO, &master_set);
+    FD_SET(udp_conn, &master_set);
+
+    while (1) {
+        rd_set = master_set;
+
+        select(max_sd + 1, &rd_set, NULL, NULL, NULL);
+
+        if (FD_ISSET(0, &rd_set)) {
+            memset(buffer, 0, 1024);
+            read(0, buffer, 1024);
+            sendto(udp_conn, buffer, strlen(buffer), 0, (struct sockaddr *)&main_addr,
+                   sizeof(main_addr));
+        }
+
+        else if (FD_ISSET(udp_conn, &rd_set)) {
+            memset(buffer, 0, 1024);
+            recv(udp_conn, buffer, 1024, 0);
+            if (strcmp(buffer, "port\n") == 0) {
+                memset(buffer, 0, 1024);
+                sprintf(buffer, "%d", atoi(argv[1]));
+                sendto(udp_conn, buffer, strlen(buffer), 0, (struct sockaddr *)&main_addr,
+                       sizeof(main_addr));
+            }
+            else {
+                printf("UDP: %s\n", buffer);
+            }
+        }
+
+        else {
+            for (int i = 0; i <= max_sd; i++) {
+                if (FD_ISSET(i, &rd_set)) {
+                    if (i == tcp_conn) {  // new clinet
+                        int new_socket = acceptClient(tcp_conn);
+                        FD_SET(new_socket, &master_set);
+                        if (new_socket > max_sd)
+                            max_sd = new_socket;
+                        printf("New client connected. fd = %d\n", new_socket);
+                    }
+
+                    else { // client sending msg
+                        int bytes_received;
+                        bytes_received = recv(i , buffer, 1024, 0);
+                        
+                        if (bytes_received == 0) { // EOF
+                            printf("client fd = %d closed\n", i);
+                            close(i);
+                            FD_CLR(i, &master_set);
+                            continue;
+                        }
+
+                        printf("client %d: %s\n", i, buffer);
+                        memset(buffer, 0, 1024);
+                    }
+                }
+            }
+        }
+    }
+
+    return 0;
+}
