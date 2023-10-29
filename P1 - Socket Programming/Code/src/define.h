2fda1e8c38d69729aa0b22488d61b4fbbcfe84a6
Shahriar-0 <attarshahriar@gmail.com>
2023-10-29 14:39:46 -0400

    add data structures

--- P1 - Socket Programming/Code/src/define.h
+++ P1 - Socket Programming/Code/src/define.h
@@ -17,14 +17,79 @@
 #include <unistd.h>
 
 // Constants
-#define BUF_NAME 32
-#define BUF_PNAME 64
-#define BUF_CLI 128
-#define BUF_MSG 512
-#define BCAST_IP "255.255.255.255"
-#define TIMEOUT 90
+#define BUF_NAME       32
+#define BUF_PNAME      64
+#define BUF_CLI        128
+#define BUF_MSG        512
+#define BCAST_IP       "255.255.255.255"
+#define TIMEOUT        90
+#define RECIPE_ADDRESS "../../../Assets/recipes.json"
+#define MAX_INGREDIENT 20
+#define MAX_SUPPLIER   20
+#define MAX_RESTAURANT 20
+#define MAX_FOOD       20
+#define MAX_REQUEST    20
+
 
 // Structs
-// TODO
+typedef char* Ingredient;
+
+typedef struct {
+    char name[BUF_NAME];
+    Ingredient ingredients[MAX_INGREDIENT];
+    int quantity[MAX_INGREDIENT];
+    int ingredientSize;
+} Food;
+
+typedef struct {
+    int fd;
+    struct sockaddr_in addr;
+} BroadcastInfo;
+
+typedef struct {
+    char name[BUF_NAME];
+    unsigned short port;
+} BroadcastData;
+
+typedef struct {
+    char name[BUF_PNAME];
+    BroadcastInfo bcast;
+    unsigned short tcpPort;
+} Supplier;
+
+typedef enum {
+    OPEN = 0,
+    CLOSED = 1
+} RestaurantState;
+
+typedef struct {
+    char customerName[BUF_NAME];
+    int customerPort;
+    char foodName[BUF_NAME];
+} FoodRequest;
+
+typedef struct {
+    char name[BUF_NAME];
+    BroadcastData suppliers[MAX_SUPPLIER];
+    int supplierSize;
+    Food menu[MAX_FOOD];
+    int menuSize;
+    Ingredient ingredients[MAX_INGREDIENT];
+    int ingredientSize;
+    RestaurantState state;
+    FoodRequest pendingRequest[MAX_REQUEST];
+    int pendingRequestSize;
+    FoodRequest handledRequests[MAX_REQUEST];
+    int handledRequestsSize;
+    unsigned short tcpPort;
+} Restaurant;
+
+
+typedef struct {
+    char name[BUF_NAME];
+    Food foods[MAX_FOOD];
+    BroadcastData restaurants[MAX_RESTAURANT];
+    int restaurantSize;
+} Customer;
 
 #endif  // DEFINE_H_INCLUDE
